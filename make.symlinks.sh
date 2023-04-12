7f56c2c669a9a584d4f9f43213fc1a1738d16a5e
TBS093A <zukkamil.44@gmail.com>
2023-04-12 15:37:40 +0200

    feat: upgrade configuration

--- make.symlinks.sh
+++ make.symlinks.sh
@@ -1,6 +1,4 @@
 ln -s $(pwd)/.gitconfig ~/.gitconfig
-ln -s $(pwd)/.bashrc ~/.bashrc
-ln -s $(pwd)/.bash_profile ~/.bash_profile
 
 ln -s $(pwd)/.zshrc ~/.zshrc
 ln -s $(pwd)/lambda-00x097.zsh-theme ~/.oh-my-zsh/themes/lambda-00x097.zsh-theme
