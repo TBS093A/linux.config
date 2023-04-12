9b5121c71dce5ab195a080ce5a6943f227a0b4e9
TBS093A <zukkamil.44@gmail.com>
2023-04-12 16:20:17 +0200

    feat: fixes

--- install.zsh.sh
+++ install.zsh.sh
@@ -1,1 +1,3 @@
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o zsh.install.sh
+chmod +x ./zsh.install.sh
+./zsh.install.sh
