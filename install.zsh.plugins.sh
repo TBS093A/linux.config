dc1b6b28f81049622e6109fc44338d44ae56500a
TBS093A <zukkamil.44@gmail.com>
2023-04-12 16:37:25 +0200

    feat: fixes

--- install.zsh.plugins.sh
+++ install.zsh.plugins.sh
@@ -1,5 +1,5 @@
 source /root/linux.config/plugins
 
 for plugin in ${ZSH_INIT_PLUGINS[@]}; do
-    git clone https://github.com/zsh-users/${ plugin }.git
+    git clone https://github.com/zsh-users/$plugin.git
 done
